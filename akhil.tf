529a538f96de10072e4f27de202f4e75a619d9ce
abhja <103754003+abhja@users.noreply.github.com>
2024-03-06 21:38:40 +0530

    Update akhil.tf

--- akhil.tf
+++ akhil.tf
@@ -1,5 +1,5 @@
 terraform {
-required_version = "~> 1.1"
+  required_version = "~> 1.1"
   required_providers {
     aws = {
       version = "~>3.27"
@@ -11,17 +11,16 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
 }
- 
 resource "aws_instance" "yourec2" {
-  ami                    = "ami-0f403e3180720dd7e"
+  ami                    = "ami-0f403e3180720dd7e "
   instance_type          = "t2.micro"
   tags = {
     Name = "terraform-inst"
-  }
+  }
 }
-
-variable "access_key" {}
-variable "secret_key" {}
+ 
+variable "access_key" { }
+variable "secret_key" { }
 variable "region_name" {
- default ="us-east-1"
+default = "us-east-1"
 }
